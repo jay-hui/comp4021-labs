8e86d87861c3de8e5a5dc0d59c6d5ead7fa67770
javier <29498758+javier-hui@users.noreply.github.com>
2022-03-25 15:20:41 +0800

    lab4 fix indents

--- lab4/bounding_box.js
+++ lab4/bounding_box.js
@@ -4,7 +4,7 @@
 // - `left` - The left side of the box
 // - `bottom` - The bottom side of the box
 // - `right` - The right side of the box
-const BoundingBox = function(ctx, top, left, bottom, right) {
+const BoundingBox = function (ctx, top, left, bottom, right) {
 
     // This is the path containing the bounding box.
     // It is initialized using the parameters of the function.
@@ -17,27 +17,27 @@ const BoundingBox = function(ctx, top, left, bottom, right) {
     path.rect(left, top, right - left, bottom - top);
 
     // This function gets the top side of the bounding box.
-    const getTop = function() {
+    const getTop = function () {
         return top;
     };
 
     // This function gets the left side of the bounding box.
-    const getLeft = function() {
+    const getLeft = function () {
         return left;
     };
 
     // This function gets the bottom side of the bounding box.
-    const getBottom = function() {
+    const getBottom = function () {
         return bottom;
     };
 
     // This function gets the right side of the bounding box.
-    const getRight = function() {
+    const getRight = function () {
         return right;
     };
 
     // This function gets the four corner points of the bounding box.
-    const getPoints = function() {
+    const getPoints = function () {
         return {
             topLeft: [left, top],
             topRight: [right, top],
@@ -48,13 +48,13 @@ const BoundingBox = function(ctx, top, left, bottom, right) {
 
     // This function tests whether a point is in the bounding box.
     // - `x`, `y` - The (x, y) position to be tested
-    const isPointInBox = function(x, y) {
+    const isPointInBox = function (x, y) {
         return ctx.isPointInPath(path, x, y);
     };
 
     // This function checks whether the two bounding boxes intersect.
     // - `box` - The other bounding box
-    const intersect = function(box) {
+    const intersect = function (box) {
         /* Check the points of the other box */
         let points = box.getPoints();
         for (const key in points) {
@@ -73,10 +73,10 @@ const BoundingBox = function(ctx, top, left, bottom, right) {
     };
 
     // This function generates a random point inside the bounding box.
-    const randomPoint = function() {
+    const randomPoint = function () {
         const x = left + (Math.random() * (right - left));
         const y = top + (Math.random() * (bottom - top));
-        return {x, y};
+        return { x, y };
     };
 
     // The methods are returned as an object here.
--- lab4/player.js
+++ lab4/player.js
@@ -3,23 +3,23 @@
 // - `x` - The initial x position of the player
 // - `y` - The initial y position of the player
 // - `gameArea` - The bounding box of the game area
-const Player = function(ctx, x, y, gameArea) {
+const Player = function (ctx, x, y, gameArea) {
 
     // This is the sprite sequences of the player facing different directions.
     // It contains the idling sprite sequences `idleLeft`, `idleUp`, `idleRight` and `idleDown`,
     // and the moving sprite sequences `moveLeft`, `moveUp`, `moveRight` and `moveDown`.
     const sequences = {
         /* Idling sprite sequences for facing different directions */
-        idleLeft:  { x: 0, y: 25, width: 24, height: 25, count: 3, timing: 2000, loop: false },
-        idleUp:    { x: 0, y: 50, width: 24, height: 25, count: 1, timing: 2000, loop: false },
+        idleLeft: { x: 0, y: 25, width: 24, height: 25, count: 3, timing: 2000, loop: false },
+        idleUp: { x: 0, y: 50, width: 24, height: 25, count: 1, timing: 2000, loop: false },
         idleRight: { x: 0, y: 75, width: 24, height: 25, count: 3, timing: 2000, loop: false },
-        idleDown:  { x: 0, y:  0, width: 24, height: 25, count: 3, timing: 2000, loop: false },
+        idleDown: { x: 0, y: 0, width: 24, height: 25, count: 3, timing: 2000, loop: false },
 
         /* Moving sprite sequences for facing different directions */
-        moveLeft:  { x: 0, y: 125, width: 24, height: 25, count: 10, timing: 50, loop: true },
-        moveUp:    { x: 0, y: 150, width: 24, height: 25, count: 10, timing: 50, loop: true },
+        moveLeft: { x: 0, y: 125, width: 24, height: 25, count: 10, timing: 50, loop: true },
+        moveUp: { x: 0, y: 150, width: 24, height: 25, count: 10, timing: 50, loop: true },
         moveRight: { x: 0, y: 175, width: 24, height: 25, count: 10, timing: 50, loop: true },
-        moveDown:  { x: 0, y: 100, width: 24, height: 25, count: 10, timing: 50, loop: true }
+        moveDown: { x: 0, y: 100, width: 24, height: 25, count: 10, timing: 50, loop: true }
     };
 
     // This is the sprite object of the player created from the Sprite module.
@@ -27,9 +27,9 @@ const Player = function(ctx, x, y, gameArea) {
 
     // The sprite object is configured for the player sprite here.
     sprite.setSequence(sequences.idleDown)
-          .setScale(2)
-          .setShadowScale({ x: 0.75, y: 0.20 })
-          .useSheet("player_sprite.png");
+        .setScale(2)
+        .setShadowScale({ x: 0.75, y: 0.20 })
+        .useSheet("player_sprite.png");
 
     // This is the moving direction, which can be a number from 0 to 4:
     // - `0` - not moving
@@ -44,7 +44,7 @@ const Player = function(ctx, x, y, gameArea) {
 
     // This function sets the player's moving direction.
     // - `dir` - the moving direction (1: Left, 2: Up, 3: Right, 4: Down)
-    const move = function(dir) {
+    const move = function (dir) {
         if (dir >= 1 && dir <= 4 && dir != direction) {
             switch (dir) {
                 case 1: sprite.setSequence(sequences.moveLeft); break;
@@ -58,7 +58,7 @@ const Player = function(ctx, x, y, gameArea) {
 
     // This function stops the player from moving.
     // - `dir` - the moving direction when the player is stopped (1: Left, 2: Up, 3: Right, 4: Down)
-    const stop = function(dir) {
+    const stop = function (dir) {
         if (direction == dir) {
             switch (dir) {
                 case 1: sprite.setSequence(sequences.idleLeft); break;
@@ -71,18 +71,18 @@ const Player = function(ctx, x, y, gameArea) {
     };
 
     // This function speeds up the player.
-    const speedUp = function() {
+    const speedUp = function () {
         speed = 250;
     };
 
     // This function slows down the player.
-    const slowDown = function() {
+    const slowDown = function () {
         speed = 150;
     };
 
     // This function updates the player depending on his movement.
     // - `time` - The timestamp when this function is called
-    const update = function(time) {
+    const update = function (time) {
         /* Update the player if the player is moving */
         if (direction != 0) {
             let { x, y } = sprite.getXY();
