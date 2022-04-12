fb7f1a7ad548621d7b822034a6c63a546f807db0
javier <29498758+javier-hui@users.noreply.github.com>
2022-04-12 18:14:43 +0800

    fixed indents

--- lab6/public/scripts/ui.js
+++ lab6/public/scripts/ui.js
@@ -1,9 +1,9 @@
-const SignInForm = (function() {
+const SignInForm = (function () {
     // This function initializes the UI
-    const initialize = function() {
+    const initialize = function () {
         // Populate the avatar selection
         Avatar.populate($("#register-avatar"));
-        
+
         // Hide it
         $("#signin-overlay").hide();
 
@@ -36,8 +36,8 @@ const SignInForm = (function() {
 
             // Get the input fields
             const username = $("#register-username").val().trim();
-            const avatar   = $("#register-avatar").val();
-            const name     = $("#register-name").val().trim();
+            const avatar = $("#register-avatar").val();
+            const name = $("#register-name").val().trim();
             const password = $("#register-password").val().trim();
             const confirmPassword = $("#register-confirm").val().trim();
 
@@ -59,12 +59,12 @@ const SignInForm = (function() {
     };
 
     // This function shows the form
-    const show = function() {
+    const show = function () {
         $("#signin-overlay").fadeIn(500);
     };
 
     // This function hides the form
-    const hide = function() {
+    const hide = function () {
         $("#signin-form").get(0).reset();
         $("#signin-message").text("");
         $("#register-message").text("");
@@ -74,9 +74,9 @@ const SignInForm = (function() {
     return { initialize, show, hide };
 })();
 
-const UserPanel = (function() {
+const UserPanel = (function () {
     // This function initializes the UI
-    const initialize = function() {
+    const initialize = function () {
         // Hide it
         $("#user-panel").hide();
 
@@ -95,17 +95,17 @@ const UserPanel = (function() {
     };
 
     // This function shows the form with the user
-    const show = function(user) {
+    const show = function (user) {
         $("#user-panel").show();
     };
 
     // This function hides the form
-    const hide = function() {
+    const hide = function () {
         $("#user-panel").hide();
     };
 
     // This function updates the user panel
-    const update = function(user) {
+    const update = function (user) {
         if (user) {
             $("#user-panel .user-avatar").html(Avatar.getCode(user.avatar));
             $("#user-panel .user-name").text(user.name);
@@ -119,18 +119,18 @@ const UserPanel = (function() {
     return { initialize, show, hide, update };
 })();
 
-const OnlineUsersPanel = (function() {
+const OnlineUsersPanel = (function () {
     // This function initializes the UI
-    const initialize = function() {};
+    const initialize = function () { };
 
     // This function updates the online users panel
-    const update = function(onlineUsers) {
+    const update = function (onlineUsers) {
         const onlineUsersArea = $("#online-users-area");
 
         // Clear the online users area
         onlineUsersArea.empty();
 
-		// Get the current user
+        // Get the current user
         const currentUser = Authentication.getUser();
 
         // Add the user one-by-one
@@ -145,43 +145,43 @@ const OnlineUsersPanel = (function() {
     };
 
     // This function adds a user in the panel
-	const addUser = function(user) {
+    const addUser = function (user) {
         const onlineUsersArea = $("#online-users-area");
-		
-		// Find the user
-		const userDiv = onlineUsersArea.find("#username-" + user.username);
-		
-		// Add the user
-		if (userDiv.length == 0) {
-			onlineUsersArea.append(
-				$("<div id='username-" + user.username + "'></div>")
-					.append(UI.getUserDisplay(user))
-			);
-		}
-	};
+
+        // Find the user
+        const userDiv = onlineUsersArea.find("#username-" + user.username);
+
+        // Add the user
+        if (userDiv.length == 0) {
+            onlineUsersArea.append(
+                $("<div id='username-" + user.username + "'></div>")
+                    .append(UI.getUserDisplay(user))
+            );
+        }
+    };
 
     // This function removes a user from the panel
-	const removeUser = function(user) {
+    const removeUser = function (user) {
         const onlineUsersArea = $("#online-users-area");
-		
-		// Find the user
-		const userDiv = onlineUsersArea.find("#username-" + user.username);
-		
-		// Remove the user
-		if (userDiv.length > 0) userDiv.remove();
-	};
+
+        // Find the user
+        const userDiv = onlineUsersArea.find("#username-" + user.username);
+
+        // Remove the user
+        if (userDiv.length > 0) userDiv.remove();
+    };
 
     return { initialize, update, addUser, removeUser };
 })();
 
-const ChatPanel = (function() {
-	// This stores the chat area
+const ChatPanel = (function () {
+    // This stores the chat area
     let chatArea = null;
 
     // This function initializes the UI
-    const initialize = function() {
-		// Set up the chat area
-		chatArea = $("#chat-area");
+    const initialize = function () {
+        // Set up the chat area
+        chatArea = $("#chat-area");
 
         // Submit event for the input form
         $("#chat-input-form").on("submit", (e) => {
@@ -194,48 +194,48 @@ const ChatPanel = (function() {
             // Post it
             Socket.postMessage(content);
 
-			// Clear the message
+            // Clear the message
             $("#chat-input").val("");
         });
- 	};
+    };
 
     // This function updates the chatroom area
-    const update = function(chatroom) {
+    const update = function (chatroom) {
         // Clear the online users area
         chatArea.empty();
 
         // Add the chat message one-by-one
         for (const message of chatroom) {
-			addMessage(message);
+            addMessage(message);
         }
     };
 
     // This function adds a new message at the end of the chatroom
-    const addMessage = function(message) {
-		const datetime = new Date(message.datetime);
-		const datetimeString = datetime.toLocaleDateString() + " " +
-							   datetime.toLocaleTimeString();
+    const addMessage = function (message) {
+        const datetime = new Date(message.datetime);
+        const datetimeString = datetime.toLocaleDateString() + " " +
+            datetime.toLocaleTimeString();
 
-		chatArea.append(
-			$("<div class='chat-message-panel row'></div>")
-				.append(UI.getUserDisplay(message.user))
-				.append($("<div class='chat-message col'></div>")
-					.append($("<div class='chat-date'>" + datetimeString + "</div>"))
-					.append($("<div class='chat-content'>" + message.content + "</div>"))
-				)
-		);
-		chatArea.scrollTop(chatArea[0].scrollHeight);
+        chatArea.append(
+            $("<div class='chat-message-panel row'></div>")
+                .append(UI.getUserDisplay(message.user))
+                .append($("<div class='chat-message col'></div>")
+                    .append($("<div class='chat-date'>" + datetimeString + "</div>"))
+                    .append($("<div class='chat-content'>" + message.content + "</div>"))
+                )
+        );
+        chatArea.scrollTop(chatArea[0].scrollHeight);
     };
 
     return { initialize, update, addMessage };
 })();
 
-const UI = (function() {
+const UI = (function () {
     // This function gets the user display
-    const getUserDisplay = function(user) {
+    const getUserDisplay = function (user) {
         return $("<div class='field-content row shadow'></div>")
             .append($("<span class='user-avatar'>" +
-			        Avatar.getCode(user.avatar) + "</span>"))
+                Avatar.getCode(user.avatar) + "</span>"))
             .append($("<span class='user-name'>" + user.name + "</span>"));
     };
 
@@ -243,7 +243,7 @@ const UI = (function() {
     const components = [SignInForm, UserPanel, OnlineUsersPanel, ChatPanel];
 
     // This function initializes the UI
-    const initialize = function() {
+    const initialize = function () {
         // Initialize the components
         for (const component of components) {
             component.initialize();
